25e244323d0508c1bb3d733572ea0aa6acfe6552
Jay <ttjho@hotmail.com>
2020-09-14 23:51:09 +0000

    used Template Literals in string

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,13 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("👍Assertion Passed: " + actual + " === " + expected);
+    console.log(`👍Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("💥Assertion Failed: " + actual + " !=== " + expected);
+    console.log(`💥Assertion Failed: ${actual} !=== ${expected}`);
   }
 };
 
 // TEST CODE
 console.log(assertEqual("Bootcamp", "Bootcamp"));
 console.log(assertEqual(1, "1"));
+
+// assertEqual("Bootcamp", "Bootcamp")
