83be318ccd75df24ae24aa8401360a2c42e81495
Jay <ttjho@hotmail.com>
2020-09-22 02:20:23 +0000

    refactored all lotide functions into index.js with dependencies

---
+++ index.js
@@ -0,0 +1,35 @@
+
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertEqual = require('./assertEqual');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten')
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const eqObjects = require('./eqObjects')
+const assertObjectsEqual = require("./assertObjectsEqual");
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly')
+
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  findKey,
+  findKeyByValue,
+  flatten,
+  takeUntil,
+  without,
+  eqObjects,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+};
+
